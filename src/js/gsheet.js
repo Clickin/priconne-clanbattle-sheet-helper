aa1f04054e97524195675cafef5af3fe5938783d
Click <z1z0b4v@gmail.com>
2019-07-11 19:49:46 +0900

    added custom error apply async at updateColors() now fetch() sends error message to user

--- src/js/gsheet.js
+++ src/js/gsheet.js
@@ -9,14 +9,16 @@ import "regenerator-runtime/runtime";
  * @typedef {string[][]} sheetValues google spreadsheet data about ranges
  */
 
+
 /**
  * @typedef {Object} infoData JSON about bosses and last updated value
  * @property {sheetValues} boss array of boss ref and name pair
  * @property {string} lastValue last updated value
+ * @property {string[]} message message about errors
  */
 
 /**
- * @typedef {Object} backgroundColor RGB data of background color
+ * @typedef {Object} backgroundColor RGB data about background color, R(0~1)G(0~1)B(0~1) format
  * @property {string|number} red
  * @property {string|number} green
  * @property {string|number} blue 
@@ -30,12 +32,81 @@ import "regenerator-runtime/runtime";
  */
 
 /**
- * @typedef {Map<string|numer, backgroundColor>} colorMap map key:boss ref value:color
+ * @typedef {Map<string|number, backgroundColor>} colorMap map key:boss ref value:color
  */
 
 /**
  * @typedef {Map<string, string>} userMap map key:user ref value:user name
  */
+
+/**
+ * @typedef {Object} updateColorRequest JSON about color of user input
+ * @property {Object} repeatCell color request JSON about actual cell
+ */
+
+/**
+ * @typedef {Object} repeatCell color request JSON about actual cell
+ * @property {Object} range The range to repeat the cell in
+ * @property {Object} cell grid information about cell
+ * @property {string} fields The fields that should be updated
+ */
+
+/**
+ * @typedef {Object} range The range to repeat the cell in
+ * @property {string} sheetId sheetId
+ * @property {number} startRowIndex start index of row 
+ * @property {number} endRowIndex end index of row
+ * @property {number} startColumnIndex start index of column
+ * @property {number} endColumnIndex end index of column
+ */
+
+/**
+ * @typedef {Object} userEnteredFormat grid information about user enterd data
+ * @property {Object} backgroundColor R G B data about background color, R(0~1)G(0~1)B(0~1) format
+ */
+
+
+
+
+/**@class custom no spreadsheet error */ 
+class SpreadsheetNotExistError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this)
+    this.name = "SpreadsheetNotExistError"
+    this.message = "There is no spreadsheet has such name"
+  }
+}
+
+/**@class custom no value error */
+class GetValueError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this)
+    this.name = "GetValueError"
+    this.message = "Failed to get value from the sheet"
+  }
+}
+
+/**@class cutome failed on update error */
+class UpdateError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this)
+    this.name = "UpdateError"
+    this.message = "Failed to update value at thesheet"
+  }
+}
+
+class InvalidSpreadsheetIdError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this)
+    this.name = "InvalidSpreadsheetIdError"
+    this.message = "Entered invalid spreadsheet ID"
+    this.code = 400
+  }
+}
 
 /**@class sheet interface */
 class gsheet {
@@ -84,7 +155,7 @@ class infoSheet extends gsheet
       })
       return data
     } catch (err) {
-      throw new Error(err)
+      throw new GetValueError(err)
     } 
   }
 
@@ -94,35 +165,99 @@ class infoSheet extends gsheet
    */
   async getLastValue() {
     try {
-      const data = await this.sheets.spreadsheets.values.get({
+      const post = await this.sheets.spreadsheets.values.get({
         spreadsheetId: this.spreadsheetId,
         range: this.date + "!J2",
         majorDimension: "ROWS"
       })
-      return data
+      return post.data.values[0][0]
     } catch (err) {
-      throw new Error(err)
+      throw new GetValueError(err)
     }
   }
+  /**
+   * @description check spreadsheet existance
+   * @returns {boolean} if spreadsheet id is correct and corresponding sheet exists, retuns true
+   */
+  async checkSpreadsheet() {
+    try {
+      const res = await this.sheets.spreadsheets.get({
+        spreadsheetId: this.spreadsheetId
+      })
+      let check = []
+      for (let sheet of res.data.sheets) {
+        check.push(sheet.properties.title)
+      }
+      if (check.includes(this.date)) {
+        return true
+      }
+    } catch (err) {
+      //console.log(err)
+      if (err.code == 404) {
+        throw new InvalidSpreadsheetIdError(err)
+      }
+      else if (err instanceof ReferenceError) {
+        throw new SpreadsheetNotExistError(err)
+      }
+      else {
+        throw new Error(err)
+      }
+    }
+  }
+
   /**
    * @description fetch boss information and last updated value
    * @returns {infoData}
    */
   async fetch() {
+    let message = []
+    let spreadsheetCheck = false
     try {
-      const bossPOST = await this.getBosses()
-      const lastPOST = await this.getLastValue()
-      const bossArray = bossPOST.data.values
-      const lastValue = lastPOST.data.values[0][0]
-
-      return {
-        boss: bossArray,
-        lastValue: lastValue
+      spreadsheetCheck = await this.checkSpreadsheet()
+    } catch (err) {
+      if (err instanceof InvalidSpreadsheetIdError) {
+        message.push("잘못된 시트 URL")
+      }
+      else if (err instanceof SpreadsheetNotExistError) {
+        message.push("해당 날짜 개별 시트 없음")
+      }
+      else {
+        message.push("API 키 만료로 재로그인 바람")
+      }
+    }
+    let bossArray = [[]]
+    let lastValue
+    if (spreadsheetCheck) {
+      try {
+        const bossPOST = await this.getBosses()
+        bossArray = bossPOST.data.values
+      } catch (err) {
+        if (err instanceof GetValueError) {
+          bossArray = [["보스 정보를 입력해주세요"]]
+          message.push("보스 정보 미입력")
+        }
       }
 
-    } catch (err) {
-      throw new Error(err)
+      try {
+        lastValue = await this.getLastValue()
+      } catch (err) {
+        if (err instanceof GetValueError) {
+          lastValue = "마지막 입력값이 존재하지 않습니다"
+          message.push("마지막 입력값 없음")
+        }
+      }
+      return {
+        boss: bossArray,
+        lastValue: lastValue,
+        message: message
+      }    
     }
+    else {
+      return {
+        message: message
+      }
+    }
+    
   }
 }
 /** 
@@ -157,27 +292,59 @@ class updateSheet extends gsheet {
   }
 
   /**
+   * @description function that makes request object about color 
+   * @param {string} sheetId individual spreadsheet id
+   * @param {number} rowIndex start row index of cell
+   * @param {number} columnIndex start column index of cell
+   * @param {backgroundColor} backgroundColor background color of cell
+   * @returns {repeatCell}
+   */
+  async makeColorRequestObject(sheetId, rowIndex, columnIndex, backgroundColor) {
+    return {
+      repeatCell: {
+        range: {
+          sheetId: sheetId,
+          startRowIndex: (rowIndex+1),
+          endRowIndex: (rowIndex+2),
+          startColumnIndex: columnIndex,
+          endColumnIndex: (columnIndex+1)
+        },
+        cell: {
+          userEnteredFormat: {
+            backgroundColor: backgroundColor
+          }
+        },
+        fields: 'userEnteredFormat'
+      }
+    }
+  }
+
+  /**
    * @description get color of boss and map corresponding
    * @returns {colorMap} map of boss ref and background color
    */
   async getColors() {
-    const res = await this.sheets.spreadsheets.get({
-      spreadsheetId: this.spreadsheetId,
-      includeGridData: true,
-      ranges: "색지정!A2:A6" 
-    }) // get color setted at A2:A6
-    const data = await res.data.sheets[0].data[0].rowData
-    let colorMap = new Map()
-    for (let row of data) {
-      colorMap.set(row.values[0].formattedValue, row.values[0].userEnteredFormat.backgroundColor)
+    try {
+      const res = await this.sheets.spreadsheets.get({
+        spreadsheetId: this.spreadsheetId,
+        includeGridData: true,
+        ranges: "색지정!A2:A6" 
+      }) // get color setted at A2:A6
+      const data = res.data.sheets[0].data[0].rowData
+      let colorMap = new Map()
+      for (let row of data) {
+        colorMap.set(row.values[0].formattedValue, row.values[0].userEnteredFormat.backgroundColor)
+      }
+      return colorMap
+    } catch (err) {
+      throw new GetValueError(err)
     }
-    return colorMap
   }
 
   /**
    * @description update colors to target date sheet
    * @param {sheetValues} merged merged value of orignal values and updated values
-   * @param {colorMap} colorMap
+   * @param {colorMap} colorMap map key:boss ref value:color
    */
   async updateColors(colorMap, merged) {
     const res = await this.sheets.spreadsheets.get({
@@ -185,30 +352,18 @@ class updateSheet extends gsheet {
       ranges: this.date
     })
     const sheetId = await res.data.sheets[0].properties.sheetId //because batchUpdate for sheet needs sheet id and index number range notation
-    
+    /**
+     * request array of repeat cell
+     * @type {repeatCell[]} 
+     */
     let request = []
-    merged.forEach((row, i) => { // rowindex == i + 1
-      row.forEach((cell, j) => { // columnindex == j
+    merged.forEach(async (row, i) => { // rowindex == i + 1
+      row.forEach(async (cell, j) => { // columnindex == j
         for (let item of this.list) {
           if (item.damage === cell) {
             //console.log(item.boss)
-            request.push({
-              repeatCell: {
-                range: {
-                  sheetId: sheetId,
-                  startRowIndex: (i+1),
-                  endRowIndex: (i+2),
-                  startColumnIndex: j,
-                  endColumnIndex: (j+1)
-                },
-                cell: {
-                  userEnteredFormat: {
-                    backgroundColor: colorMap.get('' + item.boss)
-                  }
-                },
-                fields: 'userEnteredFormat'
-              }
-            })
+            const requestcell = await this.makeColorRequestObject(sheetId, i, j, colorMap.get('' + item.boss))
+            request.push(requestcell)
           }
         }
       })
@@ -223,7 +378,7 @@ class updateSheet extends gsheet {
       })
       //console.log(answer)
     } catch (err) {
-      throw new Error(err)
+      throw new UpdateError(err)
     }
   }
 
@@ -232,16 +387,17 @@ class updateSheet extends gsheet {
    * @returns {sheetValues} array of original value from google sheet
    */
   async getCurrentValues() {
-    
-    const values = await this.sheets.spreadsheets.values.batchGet({
-      spreadsheetId: this.spreadsheetId,
-      majorDimension: 'ROWS',
-      ranges: this.date + '!A2:F31'
-    })
-    //console.log(values)
-    return values.data.valueRanges[0].values
-    
-    
+    try {
+      const values = await this.sheets.spreadsheets.values.batchGet({
+        spreadsheetId: this.spreadsheetId,
+        majorDimension: 'ROWS',
+        ranges: this.date + '!A2:F31'
+      })
+      //console.log(values)
+      return values.data.valueRanges[0].values
+    } catch (err) {
+      throw new GetValueError(err)
+    }
   }
   /**
    * @description update values to target date sheet
@@ -282,7 +438,7 @@ class updateSheet extends gsheet {
       return merged
       //console.log(res)
     } catch (err) { 
-      throw new Error(err)
+      throw new UpdateError(err)
     }
   }
   /**
@@ -290,17 +446,21 @@ class updateSheet extends gsheet {
    * @returns {userMap} returns mapped data
   */
   async getUserMap() {
-    const values = await this.sheets.spreadsheets.values.batchGet({
-      spreadsheetId: this.spreadsheetId,
-      majorDimension: 'ROWS',
-      ranges: this.date + '!A2:B31'
-    })
-    let userMap = new Map()
-    for (let row of values.data.valueRanges[0].values) {
-      userMap.set(row[0], row[1])
+    try {
+      const values = await this.sheets.spreadsheets.values.batchGet({
+        spreadsheetId: this.spreadsheetId,
+        majorDimension: 'ROWS',
+        ranges: this.date + '!A2:B31'
+      })
+      let userMap = new Map()
+      for (let row of values.data.valueRanges[0].values) {
+        userMap.set(row[0], row[1])
+      }
+      return userMap
+    } catch (err) {
+      throw new GetValueError(err)
     }
     
-    return userMap
   }
   /**
    * @description update sheet using data retreived from POST
